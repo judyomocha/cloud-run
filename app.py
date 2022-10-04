f793a1007776b1230a35fa439ea47f959c62a2e9
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-04 22:29:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,43 +15,42 @@
 import signal
 import sys
 from types import FrameType
-
 from flask import Flask
-
 from utils.logging import logger
-
 app = Flask(__name__)
+from google.oauth2.service_account import Credentials
+import gspread
+import discord
+import json
+import os
+from dotenv import load_dotenv
+# .envファイルの内容を読み込見込む
+load_dotenv()
+TOKEN = os.environ['TOKEN']
+GUILD_ID = os.environ['GUILD_ID']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
+GCP_SA_KEY = os.environ['GCP_SA_KEY']
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+parsed = json.loads(GCP_SA_KEY)
+gc = gspread.service_account_from_dict(parsed)
+wb = gc.open_by_key(SPREADSHEET_KEY)
+ws = wb.sheet1
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 
-@app.route("/")
-def hello() -> str:
-    # Use basic logging with custom fields
-    logger.info(logField="custom-entry", arbitraryField="custom-entry")
+@client.event
+async def on_message(message):  # メッセージを受け取ったときの挙動
+    if message.author.bot:  # Botのメッセージは除く
+        return
+    print(message.content)
+    worksheet_list = wb.worksheets()
+    #　1つ目のシートのセル(1,1)をDiscordに送ったメッセージ内容で更新
+    worksheet_list[0].update_cell(1, 1, message.content)
+    await message.channel.send('更新しました')
 
-    # https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Child logger with trace Id.")
-
-    return "Hello, World!"
-
-
-def shutdown_handler(signal_int: int, frame: FrameType) -> None:
-    logger.info(f"Caught Signal {signal.strsignal(signal_int)}")
-
-    from utils.logging import flush
-
-    flush()
-
-    # Safely exit program
-    sys.exit(0)
-
-
-if __name__ == "__main__":
-    # Running application locally, outside of a Google Cloud Environment
-
-    # handles Ctrl-C termination
-    signal.signal(signal.SIGINT, shutdown_handler)
-
-    app.run(host="localhost", port=8080, debug=True)
-else:
-    # handles Cloud Run container termination
-    signal.signal(signal.SIGTERM, shutdown_handler)
+client.run(TOKEN)
