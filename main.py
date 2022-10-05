8b260948f658622fd005156ecfb96d32d510dd31
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-05 12:20:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ GUILD_ID = os.environ['GUILD_ID']
 CHANNEL_ID = os.environ['CHANNEL_ID']
 SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
 GCP_SA_KEY = os.environ['GCP_SA_KEY']
+SHEET_NAME = os.environ['SHEET_NAME']
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 parsed = json.loads(GCP_SA_KEY)
 gc = gspread.service_account_from_dict(parsed)
-wb = gc.open_by_key(SPREADSHEET_KEY)
-ws = wb.sheet1
+sh = gc.open_by_key(SPREADSHEET_KEY)
+ws = sh.worksheet(SHEET_NAME)
+list_of_lists = ws.get_all_values()
+i = len(list_of_lists[0])
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,13 +27,16 @@ client = discord.Client(intents=intents)
 
 
 @client.event
-async def on_message(message):  # メッセージを受け取ったときの挙動
-    if message.author.bot:  # Botのメッセージは除く
+async def on_message(message):
+    if message.author == client.user:
         return
-    print(message.content)
-    worksheet_list = wb.worksheets()
-    #　1つ目のシートのセル(1,1)をDiscordに送ったメッセージ内容で更新
-    worksheet_list[0].update_cell(1, 1, message.content)
-    await message.channel.send('更新しました')
+
+    if message.channel.id != int(CHANNEL_ID):
+        return
+
+    if message.channel.id == int(CHANNEL_ID):
+        ws.update_cell(i,1,message.content )
+        print(f'更新します @{message.author}!') 
+        await message.channel.send(f'更新します {message.author}!') 
 
 client.run(TOKEN)
