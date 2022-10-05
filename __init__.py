af2ba3007f97d09759941836ed32d4d6ff032d90
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-05 15:48:21 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,14 +4,9 @@
   import load_dotenv
   import requests
   import json
-  import gspread-dataframe
-  pip install gspread
-  pip install gunicorn
-  pip install gspread-pandas
-#pip install --upgrade pydata-google-auth
-#pip install google-api-python-client
-#pip install --upgrade pip
-#pip install google
-#pip install gspread
-#pip install oauth2client
-#pip install gspread-dataframe
+pip install --upgrade pydata-google-auth
+pip install google-api-python-client
+pip install --upgrade pip
+pip install google
+pip install gspread
+pip install oauth2client
