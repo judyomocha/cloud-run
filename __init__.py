6a1c4402341dade9f6700c9549d5983f57832cd8
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-05 12:34:57 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@
   import requests
   import json
   import gspread
+  import gspread-dataframe
 pip install --upgrade pydata-google-auth
 pip install google-api-python-client
 pip install --upgrade pip
@@ -12,3 +13,4 @@ pip install google
 pip install gspread
 pip install oauth2client
 pip install gspread-dataframe
+pip install gdata
